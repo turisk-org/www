f5a9a8d29cabd22b45bdac78ef8f92f6e04a933b
Ben Yassour <benami.yassour@gmail.com>
2024-08-31 20:58:08 +0300

    fix loading the nav bar

--- js/scripts.js
+++ js/scripts.js
@@ -7,37 +7,36 @@
 const scriptElement = document.currentScript; // Get the current script element
 const nav_bar_file = scriptElement.dataset.nav_bar_file;
 
-fetch(nav_bar_file)
+document.addEventListener('DOMContentLoaded', (event) => {
+  fetch(nav_bar_file)
     .then(response => response.text())
     .then(data => {
-        document.getElementById('menu-container').innerHTML = data;
-    });
+      document.getElementById('menu-container').innerHTML = data;
 
-window.addEventListener('DOMContentLoaded', () => {
-    let scrollPos = 0;
-    const mainNav = document.getElementById('mainNav');
-    const headerHeight = mainNav.clientHeight;
-    window.addEventListener('scroll', function() {
+      let scrollPos = 0;
+      const mainNav = document.getElementById('mainNav');
+      const headerHeight = mainNav.clientHeight;
+      window.addEventListener('scroll', function() {
         const currentTop = document.body.getBoundingClientRect().top * -1;
         if ( currentTop < scrollPos) {
-            // Scrolling Up
-            if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
-                mainNav.classList.add('is-visible');
-            } else {
-                console.log(123);
-                mainNav.classList.remove('is-visible', 'is-fixed');
-            }
+          // Scrolling Up
+          if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
+            mainNav.classList.add('is-visible');
+          } else {
+            console.log(123);
+            mainNav.classList.remove('is-visible', 'is-fixed');
+          }
         } else {
-            // Scrolling Down
-            mainNav.classList.remove(['is-visible']);
-            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
-                mainNav.classList.add('is-fixed');
-            }
+          // Scrolling Down
+          mainNav.classList.remove(['is-visible']);
+          if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
+            mainNav.classList.add('is-fixed');
+          }
         }
         scrollPos = currentTop;
+      });
     });
-})
-
+});
 
 
 
